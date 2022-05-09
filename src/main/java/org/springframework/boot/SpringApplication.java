6618d3fa62d9d6184a9befd5778cee05b3c088e4
alansolit <alancard2009@hotmail.com>
2022-05-09 20:04:34 -0300

    outras atualizaçoes

---
+++ src/main/java/org/springframework/boot/SpringApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot;
+
+import com.dio.santander.bankline.api.BanklineApiApplication;
+
+public class SpringApplication {
+
+	public static void run(Class<BanklineApiApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
